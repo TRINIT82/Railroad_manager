82d1fdb275c86146cc64032b2753931cd15a0e00
Ray <161944808+TRINIT82@users.noreply.github.com>
2026-08-05 13:39:27 +0300

    Implement coupler connection and disconnection logic

---
+++ coupler.lua
@@ -0,0 +1,76 @@
+local loco = workspace.Chassis.loco_coupler
+local car = workspace.ChassisCopy.car_coupler
+
+local Couple = game.ReplicatedStorage:WaitForChild("Couple")
+local UnCouple = game.ReplicatedStorage:WaitForChild("UnCouple")
+
+local Seat = workspace.Chassis.DriverSeat
+
+
+local function getOccupantPlayer()
+	if Seat.Occupant and Seat.Occupant.Parent then
+		return game.Players:GetPlayerFromCharacter(Seat.Occupant.Parent)
+	end
+	return nil
+end
+
+local function connectCouplers(player)
+	local attach0 = loco:FindFirstChild("CouplerAttachment") or Instance.new("Attachment", loco)
+	attach0.Name = "CouplerAttachment"
+	
+	local attach1 = car:FindFirstChild("CouplerAttachment") or Instance.new("Attachment", car)
+	attach1.Name = "CouplerAttachment"
+	
+	local rod = Instance.new("RodConstraint")
+	
+	rod.Attachment0 = attach0
+	rod.Attachment1 = attach1
+	
+	rod.Length = 5
+	rod.Visible = true
+	rod.Thickness = 0.5
+	rod.Parent = loco
+	rod.Name = "CouplerWeld"
+	
+	print("Connected couplers")
+	
+	if player then
+		UnCouple:FireClient(player, true)
+	end
+	
+end
+
+local function touchEvent()
+	local player = getOccupantPlayer()
+	
+	loco.Touched:Once(function(hit)
+
+		print("Hit Detected", hit.Name)
+
+		if hit == car then
+			Couple:FireClient(player, true)
+			--connectCouplers()
+		end
+	end)
+end
+
+local function detachCouplers()
+	local weld = loco:FindFirstChild("CouplerWeld")
+	
+	if weld then
+		weld:Destroy()
+	end
+end
+
+touchEvent()
+
+Couple.OnServerEvent:Connect(function(player)
+	
+	connectCouplers()
+	Couple:FireClient(player, false)
+end)
+
+UnCouple.OnServerEvent:Connect(function(player)
+	detachCouplers()
+	UnCouple:FireClient(player, false)
+end)
